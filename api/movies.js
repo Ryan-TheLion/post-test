c8d943a4be1e8554fb55bf8cafa3a6f82bdafb06
Ryan-TheLion <dae7249@nate.com>
2023-01-12 14:27:45 +0900

    feat: change api function with fetch

--- api/movies.js
+++ api/movies.js
@@ -1,4 +1,4 @@
-import axios, { HttpStatusCode } from "axios";
+import fetch from "node-fetch";
 import {
   GetMoviesRequestDto,
   GetMoviesResponseDto,
@@ -6,6 +6,9 @@ import {
 } from "../src/omdbAPI/dto";
 
 const url = `${process.env.apiUrl}/?apikey=${process.env.apiKey}`;
+const HttpStatusCode = {
+  InternalServerError: 500,
+};
 
 export default async function handler(req, res) {
   try {
@@ -14,12 +17,11 @@ export default async function handler(req, res) {
       body: req.body,
       query: req.query,
     });
-    const { data } = await axios.post(
-      url,
-      {
-        ...req.body,
-      },
-      { params: { ...GetMoviesRequestDto.from({ ...req.body }) } }
+
+    const { s, y, page } = GetMoviesRequestDto.from(req.body);
+
+    const data = await fetch(`${url}&s=${s}&y=${y}&page=${page}`).then((res) =>
+      res.json()
     );
 
     const { status, payload, message } = GetMoviesResponseDto.from(data);
